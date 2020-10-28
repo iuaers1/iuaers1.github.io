fb229b2eab0fa88cfbd0ce86e5f7e8427ccf70d1
iuaers1 <iuaers09809@gmail.com>
2020-10-28 16:11:22 +0100

    patch-02

--- js/functions.js
+++ js/functions.js
@@ -1,65 +1,3 @@
-let songs = [
-  {
-    artist: "artist name 1",
-    songName: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, soluta?"
-  },
-  {
-    artist: "artist name 2",
-    songName: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, soluta?"
-  },
-  {
-    artist: "artist name 3",
-    songName: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, soluta ?"
-  },
-  {
-    artist: "artist name 4",
-    songName: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, soluta ?"
-  },
-  {
-    artist: "artist name 4",
-    songName: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, soluta ?"
-  },
-  {
-    artist: "artist name 4",
-    songName: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, soluta ?"
-  },
-  {
-    artist: "artist name 4",
-    songName: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, soluta ?"
-  },
-  {
-    artist: "artist name 4",
-    songName: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, soluta ?"
-  },
-  {
-    artist: "artist name 4",
-    songName: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, soluta ?"
-  },
-  {
-    artist: "artist name 4",
-    songName: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, soluta ?"
-  },
-  {
-    artist: "artist name 4",
-    songName: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, soluta ?"
-  },
-  {
-    artist: "artist name 4",
-    songName: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, soluta ?"
-  },
-  {
-    artist: "artist name 4",
-    songName: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, soluta ?"
-  },
-  {
-    artist: "artist name 4",
-    songName: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quo, soluta ?"
-  },
-
-  {
-    artist: "artist name 5",
-    songName: "Testing Text"
-  }];
 
 
 function searchFunction() {
@@ -67,6 +5,7 @@ function searchFunction() {
   var filter = document.getElementById("myInput").value.toUpperCase();
   var table = document.getElementById("songTable");
   var tr = table.getElementsByTagName("tr");
+
   for (let i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td");
     for (let j = 0; j < td.length; j++) {
@@ -84,31 +23,34 @@ function searchFunction() {
 }
 
 function fill() {
-  let songTable = document.getElementById("songTable");
 
-  for (let i = 0; i < songs.length; i++) {
-    let row =
-      `<tr height="40px">
-      <td width="50%" nowrap="nowrap" align="left" class="tableData1">${songs[i].artist}</td>
-      <td width="50%" nowrap="nowrap" align="left" class="tableData2">${songs[i].songName}</td>
-    </tr>`;
-    songTable.innerHTML += row;
+  for (let i = 0; i < 15; i++) {
+    // fetch("https://randomuser.me/api", { mode: "cors" })
+    //   .then(response => response.json())
+    //   .then(characters => showCharacters(characters.results));
+
+
+    (async () => {
+      const res = await fetch(`https://www.randomuser.me/api`);
+      const data = await res.json();
+      showCharacters(data.results);
+    })();
+
+    showCharacters = characters => {
+      const charactersDiv = document.querySelector("#songTable");
+      characters.forEach(character => {
+        let row =
+          `<tr height="40px">
+          <td width="50%" nowrap="nowrap" align="left" class="tableData1">${character.name.first + " " + character.name.last}</td>
+          <td width="50%" nowrap="nowrap" align="left" class="tableData2">${character.name.first}</td>
+        </tr>`;
+
+        charactersDiv.innerHTML += row;
+        // charactersDiv.append(characterElement);
+      });
+    }
   }
 }
-
-// const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
-
-// function switchTheme(e) {
-//     if (e.target.checked) {
-//         document.documentElement.setAttribute('data-theme', 'dark');
-//     }
-//     else {
-//         document.documentElement.setAttribute('data-theme', 'light');
-//     }    
-// }
-
-// toggleSwitch.addEventListener('change', switchTheme, false);
-
 
 const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
@@ -122,6 +64,9 @@ if (currentTheme) {
 }
 
 function switchTheme(e) {
+  document.getElementById('theme-toggle-btn').innerHTML =
+    `<p class="theme-toggle-btn" id="theme-toggle-btn">Toggle ${localStorage.getItem('theme')} mode</p>`;
+
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
     localStorage.setItem('theme', 'dark');
@@ -130,6 +75,7 @@ function switchTheme(e) {
     document.documentElement.setAttribute('data-theme', 'light');
     localStorage.setItem('theme', 'light');
   }
+
   console.log("currently using: " + localStorage.getItem('theme') + " theme");
 }
 
